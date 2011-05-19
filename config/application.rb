b035743da5f24f04ee82d268432c38dccb8bb102
scavezze <nate.scavezze@gmail.com>
2011-05-19 08:09:33 -0600

    updated gem file to rails 3.07 and included lib

--- config/application.rb
+++ config/application.rb
@@ -8,15 +8,15 @@ Bundler.require(:default, Rails.env) if defined?(Bundler)
 
 module BcmsBlog
   class Application < Rails::Application
-  # BrowserCMS should serve static CMS assets (js, css, images) from the Gem
-config.serve_static_assets = true
+    # BrowserCMS should serve static CMS assets (js, css, images) from the Gem
+    config.serve_static_assets = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
-
+    config.autoload_paths += %W(#{config.root}/lib)
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
